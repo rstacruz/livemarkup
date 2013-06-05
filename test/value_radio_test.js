f84eb0b9d01c31edfdba9886f198c0c25381481a
Rico Sta. Cruz <hi@ricostacruz.com>
2013-06-06 00:58:10 +0800

    Failing tests for radio.

--- test/value_radio_test.js
+++ test/value_radio_test.js
@@ -6,17 +6,26 @@ describe('@value() radio', function() {
   beforeEach(Setup.env);
   beforeEach(setModel);
 
-  it("should work", function() {
+  beforeEach(function() {
     model.set('number', 'two');
     render(
       "<input class='c1' type='radio' name='number' value='one' @value='attr(\"number\")' />" +
       "<input class='c2' type='radio' name='number' value='two' />" +
       "<input class='c3' type='radio' name='number' value='three' />"
     );
+  });
 
+  it("should not change values", function() {
     assert.equal($('.c1').attr('value'), 'one');
     assert.equal($('.c2').attr('value'), 'two');
     assert.equal($('.c3').attr('value'), 'three');
+  });
+
+  xit('(Not working) should set the right value', function() {
+    assert.equal($(':radio').val(), 'two');
+  });
+
+  xit('reverse binding', function() {
   });
 
   // ---
